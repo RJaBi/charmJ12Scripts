1e2573261c95cd66c0c931ca3d31f5153ec35ac6
RJaBi <ryan07bignell+github@gmail.com>
2023-07-25 12:30:53 +0100

    now does the figure in the paper first

--- run/run_fits.sh
+++ run/run_fits.sh
@@ -10,14 +10,30 @@ tomlSave=AutoToml_fits_03_03_23
 echo "dir IS ${dir}"
 echo "scriptDir IS ${scriptDir}"
 
-for NT in 32 36 40 48 56 64 128; do
-#for NT in 64; do
+tomlDir=${dir}/${tomlSave}/
+mkdir -v ${tomlDir}
+
+for NT in 128 32 36 40 48 56 64; do
+#for NT in 128; do
+    #for NT in 64; do
+
+    OP=doublet_2fl
+    # Do the CCU separately as it has our plot in it
+    # Copy the example to the folder
+    particle=ccu
+    thisToml=${tomlDir}/${NT}_${OP}_${particle}.toml
+    echo cp -av ${dir}/fitTemplates/${NT}_${OP}_ccu.toml ${thisToml}
+    cp -av ${dir}/fitTemplates/${NT}_${OP}_ccu.toml ${thisToml}
+    # and now modify it with the quark content
+    find ${tomlDir} -type f -name ${NT}_${OP}_${particle}.toml -exec sed -i "s~OP~${particle}~g" {} \;
+    # and run the code
+    echo ${pythonExe} ${scriptDir}/${exe} ${thisToml}
+    ${pythonExe} ${scriptDir}/${exe} ${thisToml}
+    
     # The 3 flavour operators
     for OP in sigma12_3fl lambda_3fl; do # sigma32_3fl
 	# Make a folder to put the modified toml in
 	tomlExample=${dir}/fitTemplates/${NT}_${OP}_OP.toml
-	tomlDir=${dir}/${tomlSave}/
-	mkdir -v ${tomlDir}      
 	#for particle in uud uus uuc ssu ssc ccu ccs
 	#for particle in uds udc usc; do
 	for particle in udc usc; do
@@ -40,7 +56,7 @@ for NT in 32 36 40 48 56 64 128; do
 	#tomlDir=${dir}/${tomlSave}/
 	#mkdir -v ${tomlDir}      
 	#for particle in uud uus uuc ssu ssc ccu ccs; do
-	for particle in uuc ssc ccu ccs; do
+	for particle in uuc ssc ccs; do
 	    # Copy the example to the folder
 	    thisToml=${tomlDir}/${NT}_${OP}_${particle}.toml
 	    echo cp -av ${tomlExample} ${thisToml}
